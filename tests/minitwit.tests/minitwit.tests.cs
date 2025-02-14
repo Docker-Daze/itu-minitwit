30f7c09e6b6d9b3b4052d2ebd360029277c93cae
nikolaitilgreen <nikolai.tilgreen@gmail.com>
2025-02-14 13:40:54 +0100

    changes

--- tests/minitwit.tests/minitwit.tests.cs
+++ tests/minitwit.tests/minitwit.tests.cs
@@ -39,169 +39,59 @@ public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
         return await _client.PostAsync("/register", formData);
     }
 
+    public async Task<HttpResponseMessage> Login(string username, string password)
+    {
+        var formData = new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("Username", username),
+            new KeyValuePair<string, string>("Password", password),
+        });
+        return await _client.PostAsync("/login", formData);
+    }
+
+    public async Task RegisterAndLogin(string username, string password)
+    {
+        Register(username, password);
+        Login(username, password);
+    }
+
+    public async Task<HttpResponseMessage> Logout()
+    {
+        return await _client.GetAsync("/logout");
+    }
+
+    public async Task<HttpResponseMessage> AddMessage()
+    {
+        
+    }
+
     [Fact]
     public async void TestRegister()
     {
-        var r = await Register("user1", "Default123!");
-        var content = await r.Content.ReadAsStringAsync();
+        var response = await Register("user1", "Default123!");
+        var content = await response.Content.ReadAsStringAsync();
         Assert.Contains("You were successfully registered and can login now", content);
         
-        r = await Register("user1", "Default123!");
-        content = await r.Content.ReadAsStringAsync();
+        response = await Register("user1", "Default123!");
+        content = await response.Content.ReadAsStringAsync();
         Assert.Contains("The username is already taken", content);
         
-        r = await Register("", "Default123!");
-        content = await r.Content.ReadAsStringAsync();
+        response = await Register("", "Default123!");
+        content = await response.Content.ReadAsStringAsync();
         Assert.Contains("You have to enter a username", content);
         
-        r = await Register("meh", "");
-        content = await r.Content.ReadAsStringAsync();
+        response = await Register("meh", "");
+        content = await response.Content.ReadAsStringAsync();
         Assert.Contains("You have to enter a password", content);
         
-        r = await Register("meh", "x123!", "y123!");
-        content = await r.Content.ReadAsStringAsync();
+        response = await Register("meh", "x123!", "y123!");
+        content = await response.Content.ReadAsStringAsync();
         Assert.Contains("The two passwords do not match", content);
         
-        r = await Register("meh", "foo", null, "broken");
-        content = await r.Content.ReadAsStringAsync();
+        response = await Register("meh", "foo", null, "broken");
+        content = await response.Content.ReadAsStringAsync();
         Assert.Contains("You have to enter a valid email address", content);
-    } 
-    
-
-    [Fact]
-    public async void CanSeePublicTimeline()
-    {
-        var response = await _client.GetAsync("/public");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-        
-        Assert.Contains("Public Timeline", content);
     }
     
-    [Fact]
-    public async void CanSeeRegisterPage()
-    {
-        var response = await _client.GetAsync("/Identity/Account/Register");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content);
-        Assert.Contains("Register", content);
-    }
     
-    [Fact]
-    public async void CanSeeLogoutPage()
-    {
-        var response = await _client.GetAsync("/Identity/Account/Logout");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content);
-        Assert.Contains("Log out", content);
-    }
-
-    [Fact]
-    public async void CanSeeLoginPage()
-    {
-        var response = await _client.GetAsync("/Identity/Account/Login");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content);
-        Assert.Contains("Log in", content);
-    }
-    
-    [Theory]
-    [InlineData("Jacqualine Gilcoine")]
-    [InlineData("Johnnie Calixto")]
-    public async void CanSeePrivateTimeline(string author)
-    {
-        var response = await _client.GetAsync($"/{author}");
-        
-        if (!response.IsSuccessStatusCode)
-        {
-            var errorContent = await response.Content.ReadAsStringAsync();
-            throw new Exception($"API Error: {response.StatusCode}, {errorContent}");
-        }
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content);
-        Assert.Contains($"{author}'s Timeline", content);
-    }
-    
-    [Fact]
-    public async void IsPage1SameAsDefaultTimeline()
-    {
-        var responseHomePage = await _client.GetAsync("/");
-        var content1 = await responseHomePage.Content.ReadAsStringAsync();
-        responseHomePage.EnsureSuccessStatusCode();
-        var responseFirstPage = await _client.GetAsync("/?page=1");
-        var content2 = await responseFirstPage.Content.ReadAsStringAsync();
-        responseFirstPage.EnsureSuccessStatusCode();
-        
-        Assert.Contains("Chirp!", content1);
-        Assert.Contains("Public Timeline", content1);
-        Assert.Contains("Chirp!", content2);
-        Assert.Contains("Public Timeline", content2);
-        Assert.Equal(content1, content2);
-    }
-    
-    [Fact]
-    public async void SiteHasMorePagesAndNotSameAsSameAsDefaultTimeline()
-    {
-        var responseHomePage = await _client.GetAsync("/");
-        var content1 = await responseHomePage.Content.ReadAsStringAsync();
-        responseHomePage.EnsureSuccessStatusCode();
-        
-        var responsePage4 = await _client.GetAsync("/?page=4");
-        responsePage4.EnsureSuccessStatusCode();
-        var content2 = await responsePage4.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content1);
-        Assert.Contains("Public Timeline", content1);
-        Assert.Contains("Chirp!", content2);
-        Assert.Contains("Public Timeline", content2);
-        Assert.NotEqual(content1, content2);
-    }
-    
-    [Theory]
-    [InlineData("Jacqualine Gilcoine")]
-    [InlineData("Johnnie Calixto")]
-    public async void PrivateTimelinePage1SameAsPrivateDefault(string author)
-    {
-        var responseHomePage = await _client.GetAsync($"/{author}");
-        var content1 = await responseHomePage.Content.ReadAsStringAsync();
-        responseHomePage.EnsureSuccessStatusCode();
-        
-        var responsePage4 = await _client.GetAsync($"/{author}?page=1");
-        responsePage4.EnsureSuccessStatusCode();
-        var content2 = await responsePage4.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content1);
-        Assert.Contains($"{author}'s Timeline", content1);
-        Assert.Contains("Chirp!", content2);
-        Assert.Contains($"{author}'s Timeline", content2);
-        Assert.Equal(content1, content2);
-    }
-    
-    [Theory]
-    [InlineData("Jacqualine Gilcoine")]
-    [InlineData("Johnnie Calixto")]
-    public async void PrivateTimelinePage4NotSameAsPrivateDefault(string author)
-    {
-        var responseHomePage = await _client.GetAsync($"/{author}");
-        var content1 = await responseHomePage.Content.ReadAsStringAsync();
-        responseHomePage.EnsureSuccessStatusCode();
-        
-        var responsePage4 = await _client.GetAsync($"/{author}?page=4");
-        responsePage4.EnsureSuccessStatusCode();
-        var content2 = await responsePage4.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content1);
-        Assert.Contains($"{author}'s Timeline", content1);
-        Assert.Contains("Chirp!", content2);
-        Assert.Contains($"{author}'s Timeline", content2);
-        Assert.NotEqual(content1, content2);
-    }
 }
