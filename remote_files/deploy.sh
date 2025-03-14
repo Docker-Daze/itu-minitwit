867116831dd5b38df95811b65356da94d7ea9474
nikolaitilgreen <nikolai.tilgreen@gmail.com>
2025-03-14 15:39:05 +0100

    persist prometheus data

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -1,7 +1,6 @@
 source ~/.bash_profile
 
 echo "Starting deployment..."
-cd remote_files
 
 docker compose -f docker-compose.yml pull
 docker compose -f docker-compose.yml up -d
--- src/minitwit.core/obj/Debug/net9.0/minitwit.core.AssemblyInfo.cs
+++ src/minitwit.core/obj/Debug/net9.0/minitwit.core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minitwit.core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ea385d8953ef02d6aadc8d77fc8c9dbf4a130d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6caffc20067ff5febd73b789e0b15b054e520b")]
 [assembly: System.Reflection.AssemblyProductAttribute("minitwit.core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minitwit.core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
