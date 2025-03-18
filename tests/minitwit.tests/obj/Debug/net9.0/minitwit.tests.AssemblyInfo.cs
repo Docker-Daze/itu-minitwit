40ec01fe41846d7fa417a13818105c921314fad4
Matbhansen <maitrox@live.com>
2025-03-18 14:20:22 +0100

    UI register user test works

--- tests/minitwit.tests/obj/Debug/net9.0/minitwit.tests.AssemblyInfo.cs
+++ tests/minitwit.tests/obj/Debug/net9.0/minitwit.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minitwit.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300dc036d64a853a220d438067c1b96daedb5a41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54048bc3fe769b0ad7ff03e0469d15cee11d50e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("minitwit.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minitwit.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/minitwit.tests/playwright.test.cs
+++ tests/minitwit.tests/playwright.test.cs
@@ -1,22 +1,42 @@
+using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 
 namespace minitwit.tests;
 
 public class playwright_test : PageTest
-{                 
+{     
+    public static async Task RegisterUser(IPage page, string username = "username", string email = "name@example.com", string password = "Password123!")
+    {
+        await page.GotoAsync("localhost:5114/");
+        await page.GetByRole(AriaRole.Link, new() { Name = "sign up" }).ClickAsync();
+        await page.Locator("#Username").ClickAsync();
+        await page.Locator("#Username").FillAsync("username");
+        await page.Locator("#Email").ClickAsync();
+        await page.Locator("#Email").FillAsync("name@example.com");
+        await page.Locator("#Password").ClickAsync();
+        await page.Locator("#Password").FillAsync("Password123!");
+        await page.Locator("#Password2").ClickAsync();
+        await page.Locator("#Password2").FillAsync("Password123!");
+        await page.GetByRole(AriaRole.Button, new() { Name = "Sign Up" }).ClickAsync();
+        await page.Locator("#Username").ClickAsync();
+        await page.Locator("#Username").FillAsync("username");
+        await page.Locator("#Password").ClickAsync();
+        await page.Locator("#Password").FillAsync("Password123!");
+        await page.GetByRole(AriaRole.Button, new() { Name = "Sign In" }).ClickAsync();
+    }
+
     [Test]
     public async Task _register_user_via_gui()
     {
         //Arrange
-        //await Page.GotoAsync("https://localhost:5114/");
-        await Page.GotoAsync("https://www.google.com/");
+        await Page.GotoAsync("localhost:5114/");
          
         //Act
-        //await Page.Locator("#Message").ClickAsync();
+        await RegisterUser(Page, username: "username", email: "name@example.com", password: "Password123!");
 
         //Assert
-        //await Expect(Page.GetByText("minitwit")).ToBeVisibleAsync();
+        await Expect(Page.GetByText("You were logged in")).ToBeVisibleAsync();
         Assert.IsNotEmpty(await Page.ContentAsync());
 
     }
