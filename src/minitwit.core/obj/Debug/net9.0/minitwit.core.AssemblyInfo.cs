af4fde3eebb157792873c25e2de3577796c3c40c
Matbhansen <maitrox@live.com>
2025-03-18 14:24:59 +0100

    files

--- src/minitwit.core/obj/Debug/net9.0/minitwit.core.AssemblyInfo.cs
+++ src/minitwit.core/obj/Debug/net9.0/minitwit.core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minitwit.core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54048bc3fe769b0ad7ff03e0469d15cee11d50e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ec01fe41846d7fa417a13818105c921314fad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("minitwit.core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minitwit.core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
