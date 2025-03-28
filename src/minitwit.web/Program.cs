b388d9c75eb1719cbb63d828b506738ce90b191d
nikolaitilgreen <nikolai.tilgreen@gmail.com>
2025-03-28 12:26:54 +0100

    changed log level to warning

--- src/minitwit.web/Program.cs
+++ src/minitwit.web/Program.cs
@@ -39,7 +39,7 @@ builder.Configuration.AddUserSecrets<Program>()
 var outputTemplate = "{Timestamp:dd-MM-YYYY HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}";
 
 builder.Host.UseSerilog((context, services, configuration) => configuration
-    .MinimumLevel.Warning()
+    .MinimumLevel.Error()
     .ReadFrom.Configuration(context.Configuration)
     .Enrich.FromLogContext()
     .WriteTo.TCPSink("tcp://logstash:5012", new MessageTemplateTextFormatter(outputTemplate))
