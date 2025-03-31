5a6aa00f9e7e689b5d0875ac81d06f8e3d463adf
Rasmus Mygind Poulsen <rpou@itu.dk>
2025-03-31 17:52:45 +0200

    more detailed logging2

---
+++ src/minitwit.web/LogEnrichmentMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace minitwit.web;
+
+public class LogEnrichmentMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public LogEnrichmentMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var endpoint = context.Request.Path;
+        var method = context.Request.Method;
+        var requestId = context.TraceIdentifier;
+
+        using (Serilog.Context.LogContext.PushProperty("RequestId", requestId))
+        using (Serilog.Context.LogContext.PushProperty("Endpoint", endpoint))
+        using (Serilog.Context.LogContext.PushProperty("Method", method))
+        {
+            await _next(context);
+        }
+    }
+}
