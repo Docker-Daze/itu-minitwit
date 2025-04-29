1e54ad4bce3cb0902de910696808a60c8a691c79
rasmus <rasmusmp50@gmail.com>
2025-04-29 12:31:25 +0200

    add namespace to logoutmodel

--- src/minitwit.web/Pages/Logout.cshtml.cs
+++ src/minitwit.web/Pages/Logout.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using minitwit.core;
-
+namespace minitwit.web.Pages;
 [IgnoreAntiforgeryToken]
 public class LogoutModel : PageModel
 {
